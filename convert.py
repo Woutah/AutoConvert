9c90aebc9ffdd51f84160f5e744e345d30792c61
williamcorsel <c.w.corsel@umail.leidenuniv.nl>
2021-04-20 21:26:54 +0200

    some convert code

---
+++ convert.py
@@ -0,0 +1,17 @@
+import os
+
+from spect_converter import SpectConverter
+
+# audio file directory
+input_dir = './input' # TODO: to config file
+
+# spectrogram directory
+output_dir = './spmel' # TODO: to config file
+
+spect_converter = SpectConverter()
+
+spect_converter.wav_to_spect(input_dir, output_dir)
+
+
+    
+
